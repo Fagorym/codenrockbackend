f86e61c07fc6199c885b3e1bd578dc65a875c977
Fagor <oleg-veber@mail.ru>
2023-04-02 09:56:30 +0700

    create achievement migrations for inserting

---
+++ src/main/resources/db/migration/V7__insert_achievements.sql
@@ -0,0 +1,20 @@
+insert into achievement(id, name, description, max_score)
+values (0, 'Новичок', 'Выполнить 15 заданий', '15');
+
+insert into achievement(id, name, description, max_score)
+values (1, 'Бывалый', 'Выполнить 30 заданий', '30');
+
+insert into achievement(id, name, description, max_score)
+values (5, 'Ветеран', 'Выполнить 60 заданий', '60');
+
+insert into achievement(id, name, description, max_score)
+values (4, 'Легенда', 'Выполнить 120 заданий', '120');
+
+insert into achievement(id, name, description, max_score)
+values (2, 'Спичка', 'Выполнить 2 задания подряд', '2');
+
+insert into achievement(id, name, description, max_score)
+values (3, 'В огне', 'Выполнить 4 задания подряд', '4');
+
+insert into achievement(id, name, description, max_score)
+values (6, 'Устроил пожар', 'Выполнить 8 задания подряд', '8');
